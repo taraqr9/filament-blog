391bdd5889f0e7173badf529264f91d46dab887c
MD.Taraq Rahman <taraqr9@gmail.com>
2025-03-09 10:20:18 +0600

    Admin Panel: Blog list filter added

--- app/Filament/Resources/BlogResource/Pages/ListBlogs.php
+++ app/Filament/Resources/BlogResource/Pages/ListBlogs.php
@@ -2,13 +2,46 @@
 
 namespace App\Filament\Resources\BlogResource\Pages;
 
+use App\Enums\BlogStatus;
 use App\Filament\Resources\BlogResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListBlogs extends ListRecords
 {
     protected static string $resource = BlogResource::class;
+
+    public function mount(): void
+    {
+        parent::mount();
+
+        $this->activeTab = session('postActiveTab', $this->getDefaultActiveTab());
+    }
+
+    public function updatedActiveTab(): void
+    {
+        parent::updatedActiveTab();
+        session(['postActiveTab' => $this->activeTab]);
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->icon('heroicon-c-list-bullet'),
+            BlogStatus::Published->value => Tab::make()
+                ->icon('heroicon-o-check-circle')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', BlogStatus::Published)),
+            BlogStatus::Private->value => Tab::make()
+                ->icon('heroicon-s-bookmark-square')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', BlogStatus::Private)),
+            BlogStatus::Draft->value => Tab::make()
+                ->icon('heroicon-o-lock-closed')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', BlogStatus::Draft)),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
