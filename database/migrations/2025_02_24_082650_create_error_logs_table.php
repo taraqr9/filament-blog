edfa7a752e7cf834a97690c446bbece1a63b7ec7
MD.Taraq Rahman <taraqr9@gmail.com>
2025-04-13 14:25:01 +0600

    Make error fields nullable for production

--- database/migrations/2025_02_24_082650_create_error_logs_table.php
+++ database/migrations/2025_02_24_082650_create_error_logs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('error_logs', function (Blueprint $table) {
             $table->id();
             $table->string('message')->nullable();
-            $table->string('value')->nullable();
+            $table->longText('value')->nullable();
             $table->string('file')->nullable();
             $table->string('line')->nullable();
             $table->foreignId('created_by')->nullable()->constrained('users');
