1812996e6bfa6c9e6444aee7af9de4dabf9b0aa0
MD.Taraq Rahman <taraqr9@gmail.com>
2025-04-13 15:58:46 +0600

    update for production

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,2 +1,6 @@
 {{--<link rel="stylesheet" href="{{ asset('css/all.min.css') }}">--}}
 @vite('resources/css/app.css')
+
+{{--for production--}}
+{{--<link rel="stylesheet" href="{{ asset('build/assets/app-DpObo_gX.css') }}">--}}
+{{--<script type="module" src="{{ asset('build/assets/app-D03Ay3w-.js') }}"></script>--}}
