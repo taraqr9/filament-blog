e8bb8c341ebd91d24ee57ae8748ab1b62a1ca2fa
MD.Taraq Rahman <taraqr9@gmail.com>
2025-04-13 16:07:21 +0600

    update for production

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,6 +1,10 @@
 {{--<link rel="stylesheet" href="{{ asset('css/all.min.css') }}">--}}
-@vite('resources/css/app.css')
+@if (app()->environment('local'))
+    {{-- Local environment: Use Vite --}}
+    @vite('resources/css/app.css')
+@else
+    {{-- Production: Use precompiled assets --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DpObo_gX.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-D03Ay3w-.js') }}"></script>
+@endif
 
-{{--for production--}}
-{{--<link rel="stylesheet" href="{{ asset('build/assets/app-DpObo_gX.css') }}">--}}
-{{--<script type="module" src="{{ asset('build/assets/app-D03Ay3w-.js') }}"></script>--}}
