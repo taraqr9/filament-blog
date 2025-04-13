3912ca1f8443ff412da3be9318ef2fcc720bb7db
MD.Taraq Rahman <taraqr9@gmail.com>
2025-04-13 16:24:51 +0600

    update project name

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="{{ asset('images/home.jpg') }}">
-    <title> {{ isset($page_title)? $page_title : 'Blog' }} </title>
+    <title> {{ isset($page_title)? $page_title : config('app.name') }} </title>
     <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
 
     @include('partials.styles')
